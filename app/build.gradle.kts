d9578699fc985505c39189afd301950598c27881
XDan <2007daniel.john.norton@gmail.com>
2026-07-10 16:53:33 +0100

    Increment to v0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 6
-        versionName = "v0.1.6"
+        versionName = "v0.1.7-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
