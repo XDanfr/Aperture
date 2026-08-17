38e885bb270d15182784f3defef3270e4f584dde
XDan <109234186+XDanfr@users.noreply.github.com>
2026-08-17 01:48:48 +0100

    fix expressive artwork fallback imports

--- app/src/main/java/me/xdan/aperture/ui/component/ArtworkFallback.kt
+++ app/src/main/java/me/xdan/aperture/ui/component/ArtworkFallback.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.Text
+import me.xdan.aperture.ui.theme.ApertureTheme
 
 @Composable
 fun ArtworkFallback(
