cf1b11a69dda944a5292e47dcf267e399360a3c6
XDan <109234186+XDanfr@users.noreply.github.com>
2026-09-22 00:48:42 +0100

    fix: import subtitle picker list items

--- app/src/main/java/me/xdan/aperture/ui/screen/player/QuickMenuPages.kt
+++ app/src/main/java/me/xdan/aperture/ui/screen/player/QuickMenuPages.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
